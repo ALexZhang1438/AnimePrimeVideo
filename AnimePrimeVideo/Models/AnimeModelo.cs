5c6089501851cc3542e3785058b3ed6188c273f3
Alex Zhang <Alexdash1438@gmail.com>
2021-06-01 11:23:54 +0200

    añadido la primera tarjeta de anime

--- AnimePrimeVideo/Models/AnimeModelo.cs
+++ AnimePrimeVideo/Models/AnimeModelo.cs
@@ -19,7 +19,7 @@ namespace AnimePrimeVideo.Models
         public string Opening { get; set; }
         public string Ending { get; set; }
         public List<int> AnimeEpiID { get; set; }
-        public List<AnimeVocesModelo> AnimeEpi { get; set; }
+        public List<AnimeEpisodiosModelo> AnimeEpi { get; set; }
         public List<int> AnimeVozID { get; set; }
         public List<AnimeVocesModelo> AnimeVoz { get; set; }
 
